010bcfe5c3fcd9eb54a174b0ff149eba1f366172
Ivan Denys <denisivan06007@gmail.com>
2025-09-15 19:24:38 +0300

    divided the project into elements

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,19 @@ import { USER_ID } from './api/todos';
 import * as postService from './api/todos';
 import { Todo } from './types/Todo';
 import classNames from 'classnames';
+import { Header } from './components/Header/Header';
+import { TodoList } from './components/TodoList/TodoList';
+import { Footer } from './components/Footer/Footer';
+import { SelectFilterValue } from './types/SelectFilterValue';
 
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [todoTitle, setTodoTitle] = useState('');
-  const [completedTodo, setCompletedTodo] = useState(false);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
-  const [selectValue, setSelectValue] = useState('all');
+  const [selectValue, setSelectValue] = useState<SelectFilterValue>(
+    SelectFilterValue.All,
+  );
   const [count, setCount] = useState(0);
 
   const titleField = useRef<HTMLInputElement>(null);
@@ -70,16 +75,20 @@ export const App: React.FC = () => {
     });
   }, [todos, selectValue]);
 
+  function toggleTodo(id: number) {
+    setTodos(currentTodos =>
+      currentTodos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  }
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
     setTodoTitle(event.target.value);
-  }
-
-  function handleChecked(event: React.ChangeEvent<HTMLInputElement>) {
-    setCompletedTodo(event.target.checked);
   }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -103,132 +112,23 @@ export const App: React.FC = () => {
       <h1 className="todoapp__title">todos</h1>
 
       <div className="todoapp__content">
-        <header className="todoapp__header">
-          <button
-            type="button"
-            data-cy="ToggleAllButton"
-            className={classNames('todoapp__toggle-all', {
-              active: todos.some(todo => todo.completed),
-            })}
-          />
-
-          <form onSubmit={handleSubmit}>
-            <input
-              data-cy="NewTodoField"
-              type="text"
-              className="todoapp__new-todo"
-              placeholder="What needs to be done?"
-              ref={titleField}
-              value={todoTitle}
-              onChange={handleInput}
-              disabled={loading}
-            />
-          </form>
-        </header>
-
-        <section className="todoapp__main" data-cy="TodoList">
-          {filteredTodos.map(todo => {
-            return (
-              <div
-                data-cy="Todo"
-                className={classNames('todo', {
-                  completed: todo.completed,
-                })}
-                key={todo.id}
-              >
-                <label className="todo__status-label">
-                  <input
-                    data-cy="TodoStatus"
-                    type="checkbox"
-                    className="todo__status"
-                    checked={todo.completed}
-                    onChange={handleChecked}
-                  />
-                </label>
-
-                <span data-cy="TodoTitle" className="todo__title">
-                  {todo.title}
-                </span>
-
-                <button
-                  type="button"
-                  className="todo__remove"
-                  data-cy="TodoDelete"
-                >
-                  ×
-                </button>
-
-                {/* overlay will cover the todo while it is being deleted or updated */}
-                <div data-cy="TodoLoader" className="modal overlay">
-                  <div className="modal-background has-background-white-ter" />
-                  <div className="loader" />
-                </div>
-              </div>
-            );
-          })}
-        </section>
+        <Header
+          todos={todos}
+          handleSubmit={handleSubmit}
+          titleField={titleField}
+          todoTitle={todoTitle}
+          handleInput={handleInput}
+          loading={loading}
+        />
+        <TodoList filteredTodos={filteredTodos} onToggle={toggleTodo} />
 
         {todos.length !== 0 && (
-          <footer
-            data-cy="Footer"
-            className={classNames('todoapp__footer', {
-              hidden: todos.length !== 0,
-            })}
-          >
-            <span className="todo-count" data-cy="TodosCounter">
-              {count} items left
-            </span>
-
-            <nav className="filter" data-cy="Filter">
-              <a
-                href="#/"
-                className={classNames('filter__link', {
-                  selected: selectValue === 'all',
-                })}
-                data-cy="FilterLinkAll"
-                onClick={() => {
-                  setSelectValue('all');
-                }}
-              >
-                All
-              </a>
-
-              <a
-                href="#/active"
-                className={classNames('filter__link', {
-                  selected: selectValue === 'active',
-                })}
-                data-cy="FilterLinkActive"
-                onClick={() => {
-                  setSelectValue('active');
-                }}
-              >
-                Active
-              </a>
-
-              <a
-                href="#/completed"
-                className={classNames('filter__link', {
-                  selected: selectValue === 'completed',
-                })}
-                data-cy="FilterLinkCompleted"
-                onClick={() => {
-                  setSelectValue('completed');
-                }}
-              >
-                Completed
-              </a>
-            </nav>
-
-            <button
-              type="button"
-              className="todoapp__clear-completed"
-              data-cy="ClearCompletedButton"
-              disabled={!todos.some(todo => todo.completed)}
-            >
-              Clear completed
-            </button>
-          </footer>
+          <Footer
+            todos={todos}
+            count={count}
+            selectValue={selectValue}
+            setSelectValue={setSelectValue}
+          />
         )}
       </div>
 
